d7f074b49a57fd3d7a0c17faf1f96bbe1002d62f
Dhairya Chavda <dhairya@intellimedianetworks.net>
2024-04-17 18:54:24 +0530

    final commit

--- shell-script.sh
+++ shell-script.sh
@@ -7,8 +7,8 @@ node_latest_tag=$(curl -s "https://registry.hub.docker.com/v2/repositories/dhair
 next_latest_tag=$(curl -s "https://registry.hub.docker.com/v2/repositories/dhairyadockerhub/ad-next/tags/" | jq -r '.results[].name | select(test("^[0-9]+$")) | tonumber' | sort -rn | head -n1)
 
 # Replace image tag in Kubernetes YAML with the latest tag
-sed -i "s|image: dhairyadockerhub/ad-next:.*$|image: dhairyadockerhub/ad-next:$mongo_latest_tag|" ad-mongo-kub.yml
-sed -i "s|image: dhairyadockerhub/ad-next:.*$|image: dhairyadockerhub/ad-next:$node_latest_tag|" ad-node-kub.yml
+sed -i "s|image: dhairyadockerhub/ad-mongo:.*$|image: dhairyadockerhub/ad-mongo:$mongo_latest_tag|" ad-mongo-kub.yml
+sed -i "s|image: dhairyadockerhub/ad-node:.*$|image: dhairyadockerhub/ad-node:$node_latest_tag|" ad-node-kub.yml
 sed -i "s|image: dhairyadockerhub/ad-next:.*$|image: dhairyadockerhub/ad-next:$next_latest_tag|" ad-next-kub.yml
 
 # Apply updated YAML to Kubernetes cluster
