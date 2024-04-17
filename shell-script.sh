a763f3edc5319216fac314dda80308f092dba37a
Dhairya Chavda <dhairya@intellimedianetworks.net>
2024-04-17 15:31:32 +0530

    many changes

--- shell-script.sh
+++ shell-script.sh
@@ -2,9 +2,9 @@ echo "INSIDE SHELL SCRIPT"
 #!/bin/bash
 
 # Query Docker Hub API to get the latest tag
-mongo_latest_tag=$(curl -s "https://registry.hub.docker.com/v2/repositories/dhairyadockerhub/ad-mongo/tags/" | jq -r '.results | map(.name) | sort | reverse | .[0]')
-node_latest_tag=$(curl -s "https://registry.hub.docker.com/v2/repositories/dhairyadockerhub/ad-node/tags/" | jq -r '.results | map(.name) | sort | reverse | .[0]')
-next_latest_tag=$(curl -s "https://registry.hub.docker.com/v2/repositories/dhairyadockerhub/ad-next/tags/" | jq -r '.results | map(.name) | sort | reverse | .[0]')
+mongo_latest_tag=$(curl -s "https://registry.hub.docker.com/v2/repositories/dhairyadockerhub/ad-mongo/tags/" | jq -r '.results[].name | select(test("^[0-9]+$")) | tonumber' | sort -rn | head -n1)
+node_latest_tag=$(curl -s "https://registry.hub.docker.com/v2/repositories/dhairyadockerhub/ad-node/tags/" | jq -r '.results[].name | select(test("^[0-9]+$")) | tonumber' | sort -rn | head -n1)
+next_latest_tag=$(curl -s "https://registry.hub.docker.com/v2/repositories/dhairyadockerhub/ad-next/tags/" | jq -r '.results[].name | select(test("^[0-9]+$")) | tonumber' | sort -rn | head -n1)
 
 # Replace image tag in Kubernetes YAML with the latest tag
 sed -i "s|image: dhairyadockerhub/ad-next:.*$|image: dhairyadockerhub/ad-next:$mongo_latest_tag|" ad-mongo-kub.yml
